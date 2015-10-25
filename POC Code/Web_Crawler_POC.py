1630ba067edee802a21599de129899c0f2c2411d
Osarumwense Omorogbe <omorogoa@mcmaster.ca>
2015-10-25 18:30:45 -0400

    Complete POC for WebCrawl, This is working features include abilty to gather all links on a page, download HTML page and download all the resources on a HTML page

---
+++ POC Code/Web_Crawler_POC.py
@@ -0,0 +1,57 @@
+__author__ = 'Abe'
+
+from bs4 import BeautifulSoup
+from pprint import pprint
+import requests
+import urlparse
+from urllib2 import urlopen
+from urllib import urlretrieve
+import os
+import sys
+
+class Web_Crawler_POC(object):
+    def __init__(self):
+        self.start = 'www.example.com'
+
+    def grabLinks(self):
+        self.start = raw_input("What website do you what to crawl (Input format = www.example.com):")
+
+        r  = requests.get("HTTP://" + self.start)
+
+        data = r.text
+
+        soup = BeautifulSoup(data)
+
+        for link in soup.find_all('a'):
+            print "Found this link on the page :" + link.get('href')
+
+    def downloadHTML(self):
+        start = raw_input("What website do you what to crawl (Input format = www.example.com):")
+        URL = "HTTP://" + start
+        soup = BeautifulSoup(urlopen(URL))
+        parsed = list(urlparse.urlparse(URL))
+        for image in soup.findAll("img"):
+            print "Image: %(src)s" % image
+            filename = image["src"].split("/")[-1]
+            outpath = os.path.join('imgs/', filename)
+
+            if image["src"].lower().startswith("http"):
+                urlretrieve(image["src"], outpath)
+            else:
+                urlretrieve(urlparse.urlunparse(parsed), outpath)
+
+        print "HTML: " + soup.title.string
+        HTMLoutpath = os.path.join('HTML/', str(soup.title.string) + ".html")
+        urlretrieve(URL,HTMLoutpath)
+
+
+if __name__ == '__main__':
+    choice = raw_input("Do you want to get all links on site (Enter : l) or download the site (Enter: d) ")
+
+    webcrawl = Web_Crawler_POC()
+
+    if choice.lower() == 'l':
+        webcrawl.grabLinks()
+
+    if choice.lower() == 'd':
+        webcrawl.downloadHTML()
