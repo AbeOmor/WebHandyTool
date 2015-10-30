504ed02019a0a87cc42e09d7cc94b41aad09b96d
Eric Lefort <leforte@mcmaster.ca>
2015-10-30 15:11:23 -0400

    Added searching for similar Strings functionality. One known issue with having a valid, close enough string at the end of the file.

--- src/Parser.py
+++ src/Parser.py
@@ -20,7 +20,7 @@ class Parser():
         data = data.lower();
         
         # Impossible for data to contain query or query is nonsensical.
-        if len(query) > len(data) or len(query) == len(data) or len(query) == 0: 
+        if len(query) > len(data) or (len(query) == len(data) and not query is data) or len(query) == 0: 
             return [];
         
         for i in range(0, len(data)):
@@ -35,3 +35,57 @@ class Parser():
                 queryIndex = 0;
                 
         return locations;
+    
+    """
+    Searches through a String for a certain phrase or term. Returns results that are close to the query as well.
+     (i.e. "ap ple" or "bpple" would be noted for "apple") Returns the starting index for all occurrences of Strings sufficiently close to
+     the query. If the query is not located, it will return an empty array.
+     @param query - The String we are looking for.
+     @param data - The String we are searching through.
+     @param distance - The size of the acceptable variation from the query.
+     @return Indexes corresponding to the beginning of the location of the String in question.
+    """
+    def searchForSimilarString(query, data, proximity):
+        locations = [];
+        queryIndex = 0;
+        distance = 0;
+        
+        data.strip();
+        query.strip();
+        query = query.lower();
+        data = data.lower();
+        
+        if len(query) > len(data) or (len(query) == len(data) and not query is data) or len(query) == 0 or proximity > (0.4) * len(query):
+            return [];
+        
+        for i in range(0, len(data)):
+            if data[i] == query[queryIndex]:
+                if queryIndex == len(query) - 1:
+                    locations.append(i - len(query) + 1);
+                    queryIndex = 0;
+                    distance = 0;
+                else:
+                    queryIndex += 1;
+            elif distance < proximity:
+                if queryIndex == len(query) - 1:
+                    locations.append(i - len(query) + 1);
+                    queryIndex = 0;
+                    distance = 0;
+                elif isWhitespace(data[i]):
+                    distance += 1;
+                else:
+                    distance += 1;
+                    queryIndex += 1;
+            else:
+                queryIndex = 0;
+                distance = 0;
+
+        return locations;
+
+    """
+     Returns true if the character passed in is a whitespace character such as tab, space or newline.
+     @param a - The character to be checked.
+     @return Whether the character is whitespace.
+    """
+    def isWhitespace(a):
+        return a == ' ' or a == '    ' or a == os.linesep;
